6d5863eb5b52387534cca8f274df31f5a2810c79
Mart Karu <karu@metal.ee>
2018-05-02 18:54:58 +0300

    Mouse event x and y can be floats when IE has zoom applied

--- src/MouseEvents.elm
+++ src/MouseEvents.elm
@@ -25,28 +25,31 @@ module MouseEvents
 Specifically, the position of mouse events can be determined precisely as
 client coordinates and element (`targetPos`) coordinates are included.
 
+
 # Example
 
-```elm
-import MouseEvents exposing (onMouseMove)
+    import MouseEvents exposing (onMouseMove)
 
-type Msg =
-    Clicked MouseEvent
+    type Msg =
+        Clicked MouseEvent
 
-div
-  [ onClick MouseMove ]
-  [ text "Example div" ]
-```
+    div
+      [ onClick MouseMove ]
+      [ text "Example div" ]
 
 The `Clicked` message will contain the coordinates of the click.
 
+
 # Data structures
+
 @docs Position
 @docs relPos
 
 @docs MouseEvent
 
+
 # Events
+
 @docs onClick
 @docs onContextMenu
 @docs onDoubleClick
@@ -61,6 +64,7 @@ The `Clicked` message will contain the coordinates of the click.
 @docs onPointerLockError
 @docs onSelect
 @docs onWheel
+
 -}
 
 import DOM exposing (Rectangle)
@@ -78,9 +82,12 @@ type alias Position =
 {-| Mouse event data. Does not include the type of the event, but information
 common to all mouse events like position.
 
+
 ## Fields
-* `clientPos`: Event position relative to the document
-* `targetPos`: Position of the element that fired the event
+
+  - `clientPos`: Event position relative to the document
+  - `targetPos`: Position of the element that fired the event
+
 -}
 type alias MouseEvent =
     { clientPos : Position
@@ -89,16 +96,15 @@ type alias MouseEvent =
 
 
 {-| Calculates the relative position of an Event.
-
 -}
 relPos : MouseEvent -> Position
 relPos ev =
     Position (ev.clientPos.x - ev.targetPos.x) (ev.clientPos.y - ev.targetPos.y)
 
 
-mouseEvent : Int -> Int -> Rectangle -> MouseEvent
+mouseEvent : Float -> Float -> Rectangle -> MouseEvent
 mouseEvent clientX clientY target =
-    { clientPos = Position clientX clientY
+    { clientPos = Position (truncate clientX) (truncate clientY)
     , targetPos = Position (truncate target.left) (truncate target.top)
     }
 
@@ -107,8 +113,8 @@ mouseEventDecoder : Decode.Decoder MouseEvent
 mouseEventDecoder =
     Decode.map3
         mouseEvent
-        (field "clientX" Decode.int)
-        (field "clientY" Decode.int)
+        (field "clientX" Decode.float)
+        (field "clientY" Decode.float)
         (field "target" DOM.boundingClientRect)
 
 
@@ -117,11 +123,12 @@ mouseEventDecoder =
 
 
 {-| A mouse event, maps to the JS event with the same name. See
-https://developer.mozilla.org/en-US/docs/Web/Events for details.
+<https://developer.mozilla.org/en-US/docs/Web/Events> for details.
 
 Note that `onMouseEnter` and other events may be prone to strange behaviour
 similar to [Z-fighting](https://en.wikipedia.org/wiki/Z-fighting) they overlap
 while no `z-index` style attribute is set.
+
 -}
 onMouseEnter : (MouseEvent -> msg) -> Html.Attribute msg
 onMouseEnter target =
@@ -129,7 +136,7 @@ onMouseEnter target =
 
 
 {-| A mouse event, maps to the JS event with the same name. See
-https://developer.mozilla.org/en-US/docs/Web/Events for details.
+<https://developer.mozilla.org/en-US/docs/Web/Events> for details.
 -}
 onMouseOver : (MouseEvent -> msg) -> Html.Attribute msg
 onMouseOver target =
@@ -137,7 +144,7 @@ onMouseOver target =
 
 
 {-| A mouse event, maps to the JS event with the same name. See
-https://developer.mozilla.org/en-US/docs/Web/Events for details.
+<https://developer.mozilla.org/en-US/docs/Web/Events> for details.
 -}
 onMouseMove : (MouseEvent -> msg) -> Html.Attribute msg
 onMouseMove target =
@@ -145,7 +152,7 @@ onMouseMove target =
 
 
 {-| A mouse event, maps to the JS event with the same name. See
-https://developer.mozilla.org/en-US/docs/Web/Events for details.
+<https://developer.mozilla.org/en-US/docs/Web/Events> for details.
 -}
 onMouseDown : (MouseEvent -> msg) -> Html.Attribute msg
 onMouseDown target =
@@ -153,7 +160,7 @@ onMouseDown target =
 
 
 {-| A mouse event, maps to the JS event with the same name. See
-https://developer.mozilla.org/en-US/docs/Web/Events for details.
+<https://developer.mozilla.org/en-US/docs/Web/Events> for details.
 -}
 onMouseUp : (MouseEvent -> msg) -> Html.Attribute msg
 onMouseUp target =
@@ -161,7 +168,7 @@ onMouseUp target =
 
 
 {-| A mouse event, maps to the JS event with the same name. See
-https://developer.mozilla.org/en-US/docs/Web/Events for details.
+<https://developer.mozilla.org/en-US/docs/Web/Events> for details.
 -}
 onClick : (MouseEvent -> msg) -> Html.Attribute msg
 onClick target =
@@ -169,7 +176,7 @@ onClick target =
 
 
 {-| A mouse event, maps to the JS event with the same name. See
-https://developer.mozilla.org/en-US/docs/Web/Events for details.
+<https://developer.mozilla.org/en-US/docs/Web/Events> for details.
 -}
 onDoubleClick : (MouseEvent -> msg) -> Html.Attribute msg
 onDoubleClick target =
@@ -177,7 +184,7 @@ onDoubleClick target =
 
 
 {-| A mouse event, maps to the JS event with the same name. See
-https://developer.mozilla.org/en-US/docs/Web/Events for details.
+<https://developer.mozilla.org/en-US/docs/Web/Events> for details.
 -}
 onContextMenu : (MouseEvent -> msg) -> Html.Attribute msg
 onContextMenu target =
@@ -185,7 +192,7 @@ onContextMenu target =
 
 
 {-| A mouse event, maps to the JS event with the same name. See
-https://developer.mozilla.org/en-US/docs/Web/Events for details.
+<https://developer.mozilla.org/en-US/docs/Web/Events> for details.
 -}
 onWheel : (MouseEvent -> msg) -> Html.Attribute msg
 onWheel target =
@@ -193,7 +200,7 @@ onWheel target =
 
 
 {-| A mouse event, maps to the JS event with the same name. See
-https://developer.mozilla.org/en-US/docs/Web/Events for details.
+<https://developer.mozilla.org/en-US/docs/Web/Events> for details.
 -}
 onMouseLeave : (MouseEvent -> msg) -> Html.Attribute msg
 onMouseLeave target =
@@ -201,7 +208,7 @@ onMouseLeave target =
 
 
 {-| A mouse event, maps to the JS event with the same name. See
-https://developer.mozilla.org/en-US/docs/Web/Events for details.
+<https://developer.mozilla.org/en-US/docs/Web/Events> for details.
 -}
 onMouseOut : (MouseEvent -> msg) -> Html.Attribute msg
 onMouseOut target =
@@ -209,7 +216,7 @@ onMouseOut target =
 
 
 {-| A mouse event, maps to the JS event with the same name. See
-https://developer.mozilla.org/en-US/docs/Web/Events for details.
+<https://developer.mozilla.org/en-US/docs/Web/Events> for details.
 -}
 onSelect : (MouseEvent -> msg) -> Html.Attribute msg
 onSelect target =
@@ -217,7 +224,7 @@ onSelect target =
 
 
 {-| A mouse event, maps to the JS event with the same name. See
-https://developer.mozilla.org/en-US/docs/Web/Events for details.
+<https://developer.mozilla.org/en-US/docs/Web/Events> for details.
 -}
 onPointerLockChange : (MouseEvent -> msg) -> Html.Attribute msg
 onPointerLockChange target =
@@ -225,7 +232,7 @@ onPointerLockChange target =
 
 
 {-| A mouse event, maps to the JS event with the same name. See
-https://developer.mozilla.org/en-US/docs/Web/Events for details.
+<https://developer.mozilla.org/en-US/docs/Web/Events> for details.
 -}
 onPointerLockError : (MouseEvent -> msg) -> Html.Attribute msg
 onPointerLockError target =
